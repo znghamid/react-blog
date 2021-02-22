dc82396a984b9dcf63d8362ca695c497c642afe3
Hamidreza Zangooie <hrzangoie@gmail.com>
2021-02-22 23:50:15 +0330

    P4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
 function App() {
+
+  const title = "First react blog";
+  const link = "http://lilnk.ir";
+  const like = 50;
+  const array = [1, 2, 3, 4, 5];
+
   return (
     <div className="App">
-      <h1>Header</h1>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>liked {like} times</p>
+        <a href={link}>lilnk.ir</a>
+        <p>{array}</p>
+      </div>
     </div>
   );
 }
