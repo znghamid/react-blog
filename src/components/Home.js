09f611a34c1d4de6ca29af802cf1c2557868e556
Hamidreza Zangooie <hrzangoie@gmail.com>
2021-02-24 16:25:17 +0330

    create blogs preview and style it

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 
 const Home = () => {
-
-  const [name, setName] = useState('hamidreza');
-  const [age, setAge] = useState(18);
-
-  const handleClick = () => {
-    setName('arash');
-    setAge(19);
-  }
-
+  const [blogs, setBlogs] = useState([
+    { title: 'create website', blog: 'lorem ipsum...', author: 'hamidreza', id: 1},
+    { title: 'how to code like a boss', blog: 'lorem ipsum...', author: 'arash', id: 2},
+    { title: 'tips and trick', blog: 'lorem ipsum...', author: 'ali', id: 3},
+    { title: 'best web dev tool', blog: 'lorem ipsum...', author: 'mohammad', id: 4}
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick} >Click me !</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
