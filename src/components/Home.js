2a401472c8cc8dcfabf5ffa700b9a52d09ada614
Hamidreza Zangooie <hrzangoie@gmail.com>
2021-02-25 17:46:49 +0330

    add loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,24 @@ import BlogList from './BlogList';
 const Home = () => {
   const [title, setTitle] = useState('All Blogs');
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-      })
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          setBlogs(data);
+          setIsLoading(false);
+        })
+    }, 2000);
   }, []);
 
   return (
     <div className="home">
+      {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title={title} />}
     </div>
   );
