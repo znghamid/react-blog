3e6796bc8aa5e87c36203869ca3ec8eb2a91b02a
Hamidreza Zangooie <hrzangoie@gmail.com>
2021-02-25 17:23:34 +0330

    feching data from json server and show it in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,26 +3,21 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [title, setTitle] = useState('All Blogs');
-  const [blogs, setBlogs] = useState([
-    { title: 'create website', blog: 'lorem ipsum...', author: 'hamidreza', id: 1 },
-    { title: 'how to code like a boss', blog: 'lorem ipsum...', author: 'arash', id: 2 },
-    { title: 'tips and trick', blog: 'lorem ipsum...', author: 'ali', id: 3 },
-    { title: 'best web dev tool', blog: 'lorem ipsum...', author: 'mohamad', id: 4 }
-  ]);
-  const handleDelete = (id) => {
-    let newBlog = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlog);
-  }
+  const [blogs, setBlogs] = useState(null);
+
   useEffect(() => {
-    console.log(`blogs lenght is ${blogs.length}`);
-    if (blogs.length === 0) {
-      setTitle('No more to show');
-    }
-  }, [blogs]);
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      })
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={title} handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} title={title} />}
     </div>
   );
 }
