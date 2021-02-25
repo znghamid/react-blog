366e6cdd3799f46f9479fc012709ce95b684b2d2
Hamidreza Zangooie <hrzangoie@gmail.com>
2021-02-25 18:11:07 +0330

    add catch to fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,22 +5,32 @@ const Home = () => {
   const [title, setTitle] = useState('All Blogs');
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [errorFetch, setErrorFetch] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
         .then(res => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
           return res.json();
         })
         .then(data => {
           setBlogs(data);
           setIsLoading(false);
+          setErrorFetch(null);
         })
-    }, 2000);
+        .catch(err => {
+          setErrorFetch(err.message);
+          setIsLoading(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {errorFetch && <div>{errorFetch}</div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title={title} />}
     </div>
